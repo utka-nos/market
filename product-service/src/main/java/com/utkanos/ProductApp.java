e0ddee1e352b2a1a22edc2f5ef62eb496b2dec42
utka-nos <dimikmusik@yandex.ru>
2022-10-31 20:41:37 +0300

    works all but not ProductApp

--- product-service/src/main/java/com/utkanos/ProductApp.java
+++ product-service/src/main/java/com/utkanos/ProductApp.java
@@ -2,12 +2,14 @@ package com.utkanos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class App {
+@EnableEurekaClient
+public class ProductApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(ProductApp.class, args);
     }
 
 }
--- product-service/src/main/java/com/utkanos/api/ProductController.java
+++ product-service/src/main/java/com/utkanos/api/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping
 public class ProductController {
 
     @Autowired
